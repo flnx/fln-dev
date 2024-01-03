e6b865257defb04f1c60c43c9beae351409c1f68
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-01-03 15:20:53 +0200

    Installing NextUi and Adding navbar :)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Navbar } from "@/components/Navbar/Navbar  ";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Nav } from '@/components/navbar/Navbar  ';
+import { Providers } from './providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: "Fln Dev",
-    description: "Web Dev Content",
+    title: 'Fln Dev',
+    description: 'Web Dev Content',
 };
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <Navbar />
-            <body className={inter.className}>{children}</body>
+        <html lang="en" className="dark">
+            <body className={inter.className}>
+                <Providers>
+                    {/* <div className="container mx-auto px-4">
+                    </div> */}
+                        <Nav />
+                        <main>{children}</main>
+                </Providers>
+            </body>
         </html>
     );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,84 @@
-export const Navbar = () => {
-    return <h1 className="text-3xl font-bold underline">Navbar</h1>;
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
+} from '@nextui-org/react';
+
+export const Nav = () => {
+    const menuItems = [
+        'Profile',
+        'Dashboard',
+        'Activity',
+        'Analytics',
+        'System',
+        'Deployments',
+        'My Settings',
+        'Team Settings',
+        'Help & Feedback',
+        'Log Out',
+    ];
+
+    return (
+        <Navbar isBordered shouldHideOnScroll maxWidth="xl">
+            <NavbarContent className="sm:hidden" justify="start">
+                <NavbarMenuToggle />
+            </NavbarContent>
+
+            <NavbarContent className="sm:hidden pr-3" justify="center">
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">ACME</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">ACME</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="end">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page" color="warning">
+                        Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Integrations
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            className="w-full"
+                            color={
+                                index === 2
+                                    ? 'warning'
+                                    : index === menuItems.length - 1
+                                      ? 'danger'
+                                      : 'foreground'
+                            }
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
 };
