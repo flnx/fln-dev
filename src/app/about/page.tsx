6a8922f9735b4643c90e944aa393faf7b78dbbc4
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-01-12 02:13:59 +0200

    Changing Showcase image intro line height

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,8 @@ const About = () => {
     <section>
       <h1 className="text-2xl font-semibold mb-5">Hi there👋</h1>
 
-      <article className="max-w-screen-md [&>p]:mb-5 text-large">
-        <p>I'm a web developer with a primary focus on the frontend. I finished my studies in SoftUni (Bulgaria) in early 2023. My favorite frameworks are React and Next.js.</p>
+      <article className="max-w-screen-xs [&>p]:mb-5 text-medium leading-relaxed" aria-label="about me">
+        <p>I'm a web developer with a primary focus on the frontend. I finished my studies in SoftUni (Bulgaria) in early 2023.</p>
         
         <p>I love working on projects that I'm passionate about. My goal is to have fun and learn new things everyday. I also enjoy reading/watching IT related stuff, sci-fi movies, listening to podcasts and audiobooks. You can also find me on the street running or in the gym.</p>
 
--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -7,18 +7,18 @@ export const Showcase = () => {
       <div className="md:flex flex-row-reverse gap-4 p-2">
         <div className="flex flex-1 gap-4 relative mb-5">
           <Image
-            src="https://res.cloudinary.com/kentcdodds-com/image/upload/w_900,q_auto,f_auto/kentcdodds.com/illustrations/kody/kody_snowboarding_flying_red"
-            width={900}
-            height={0}
+            src="/images/showcase.png"
+            width={3840}
+            height={3840}
             alt="image"
             sizes="100vw"
-            className="w-full h-auto object-contain mb-12"
+            className="w-full h-auto object-contain mb-12 rounded-b-full"
           />
         </div>
         <div className="flex-1 pt-5 md:pt-32">
           <div className="flex flex-col gap-20 lg:pr-28">
-            <h1 className="text-3xl md:text-4xl !leading-tight">
-              I'm Kaloyan, I like coding and sharing my experiences in the world of
+            <h1 className="text-3xl md:text-4xl !leading-relaxed">
+              I'm Kaloyan, I like <span className="font-bold text-secondary">coding</span> and sharing my experiences in the world of
               web development
             </h1>
             <div className="flex flex-col gap-5">
